a714d365b12243b7be0220ee70fb3c8ebc99b2cb
JitHoon <imessi0336@gmail.com>
2024-02-21 12:50:14 +0900

    feat: 채팅 db 연동

--- client/app/(chat)/_components/ChatContainer.tsx
+++ client/app/(chat)/_components/ChatContainer.tsx
@@ -2,10 +2,20 @@
 
 import React, { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
+import {
+  doc,
+  setDoc,
+  collection,
+  serverTimestamp,
+  query,
+  onSnapshot,
+  orderBy,
+} from "firebase/firestore";
 
 import InputText from "./InputText";
 import UserLogin from "./UserLogin";
 import ChatList from "./ChatList";
+import db from "@/firebase/app";
 
 const ChatContainer = () => {
   // ERROR: Module not found: Can't resolve 'bufferutil' in '/Users/jithoon/Documents/laugh-lab/client/node_modules/ws/lib'
@@ -15,10 +25,31 @@ const ChatContainer = () => {
   const [user, setUser] = useState<any>("");
   const [avatar, setAvatar] = useState<any>("");
 
+  const chatsRef = collection(db, "Messages");
+
   useEffect(() => {
     setUser(sessionStorage.getItem("user"));
     setAvatar(sessionStorage.getItem("avatar"));
   }, [user, avatar]);
+
+  // db에서 지난 대화 불러오기
+  useEffect(() => {
+    const q = query(chatsRef, orderBy("createdAt", "asc"));
+
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      const fireChats: any[] = [];
+
+      querySnapshot.forEach((doc) => {
+        fireChats.push(doc.data());
+      });
+
+      setChats([...fireChats]);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
 
   useEffect(() => {
     socketio.on("chat", (senderChats) => {
@@ -30,32 +61,43 @@ const ChatContainer = () => {
     };
   }, []);
 
-  function sendChatToSocket(chat: any) {
+  const sendChatToSocket = (chat: any) => {
     socketio.emit("chat", chat);
-  }
+  };
 
-  function addMessage(chat: any) {
+  const addToFirrebase = (chat: any) => {
+    const newChat = {
+      avatar,
+      createdAt: serverTimestamp(),
+      user,
+      message: chat.message,
+    };
+    const chatRef = doc(chatsRef);
+
+    setDoc(chatRef, newChat)
+      .then(() => console.log("Chat added succesfully"))
+      .catch(console.log);
+  };
+
+  const addMessage = (chat: any) => {
     const newChat = { ...chat, user: sessionStorage.getItem("user"), avatar };
-    //addToFirrebase(chat);
+    addToFirrebase(chat);
     setChats([...chats, newChat]);
     sendChatToSocket([...chats, newChat]);
-  }
+  };
 
-  function logout() {
+  const logout = () => {
     sessionStorage.removeItem("user");
     sessionStorage.removeItem("avatar");
     setUser("");
-  }
+  };
 
   return (
     <>
       {user ? (
         <>
           <h4>Username: {user}</h4>
-          <p
-            onClick={() => logout()}
-            style={{ color: "blue", cursor: "pointer" }}
-          >
+          <p onClick={logout} style={{ color: "blue", cursor: "pointer" }}>
             Log Out
           </p>
           <ChatList chats={chats} user={user} />
--- client/app/(chat)/_components/InputText.tsx
+++ client/app/(chat)/_components/InputText.tsx
@@ -39,6 +39,7 @@ const InputText = ({ addMessage }: Props) => {
     addMessage({
       message,
     });
+
     setMessage("");
   }
 
